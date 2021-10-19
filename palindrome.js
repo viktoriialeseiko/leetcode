e3dd4caf2216d5d43ec8619f97ff567e7be1b10c
viktoriialeseiko <vikyshaaa1@gmail.com>
2021-10-19 17:12:35 -0500

    fixed palindrome for negative numbers

--- palindrome.js
+++ palindrome.js
@@ -2,14 +2,30 @@
 
 // An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
-var isPalindrome = function(x) {
-    let len = x.length;
-    let mid = Math.floor(len/2);
+// var isPalindrome = function(x) {
+//     let len = x.length;
+//     let mid = Math.floor(len/2);
     
-    for (let i=0; i<mid; i++) {
-        if(x[i] !== x[len-1-i]) {
-            return false;
-        }
+//     for (let i=0; i<mid; i++) {
+//         if(x[i] !== x[len-1-i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+var isPalindrome = function(x) {
+    // Base condition
+    if (x < 0) {
+        return false;
     }
-    return true;
+    // Store the number in a variable
+    let number = x;
+    // This will store the reverse of the number
+    let reverse = 0;
+    while (number > 0) {
+        reverse = reverse * 10 + number % 10;
+        number = parseInt(number / 10);
+    }
+    return x === reverse;
 };
