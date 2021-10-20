9f0e64716e2ab5ddb387da3d557e0c4139bbe2c9
viktoriialeseiko <vikyshaaa1@gmail.com>
2021-10-19 20:46:43 -0500

    recursion approach was added

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -64,3 +64,23 @@ var mergeTwoLists = function (l1, l2) {
 
 
 
+// Solution #2: Recursion
+// It does essentially the same thing as the while loop, but in a much more concise way. 
+
+
+var mergeTwoLists = function (l1, l2) {
+    // If either list is empty, return the other list's node
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+
+    // If l1 is lower
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+    // If l2 is lower
+    else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+};
